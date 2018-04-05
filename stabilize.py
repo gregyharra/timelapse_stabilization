58306808c26b9cea4e6e4b9e732b4c7fc7ec9bc5
gregyharra <gregyharra@gmail.com>
2018-04-05 19:00:51 +0200

    started implementing frane rectification

---
+++ stabilize.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2
+from math import cos, sin
+
+# stabilize movie according to smooth trajectory
+def stabilize(mov, transform, BORDER_CROP = 20):
+    print "Done"
+    return stabalized, compare
