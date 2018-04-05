b24273b575949da254e64340425abd425b93150d
gregyharra <gregyharra@gmail.com>
2018-04-05 18:28:55 +0200

    find feature to track and their matching points

--- rigid_estimation.py
+++ rigid_estimation.py
@@ -27,5 +27,23 @@ def rigid_estimation(mov):
         cur = mov[t]
         gray_cur = cv2.cvtColor(cur, cv2.COLOR_BGR2GRAY)
 
-   return prev_to_cur_transform
+        # find features to track in previous frames
+        prev_corner = cv2.goodFeaturesToTrack(gray_prev, 200, 0.01, 30)
+        # calculate new coordinates of features in current frame
+        cur_corner, status, err = cv2.calcOpticalFlowPyrLK(gray_prev, gray_cur, 
+                                                           prev_corner, None, 
+                                                           **lk_params) 
+
+        # remove outliers
+        prev_corner1 = []
+        cur_corner1 = []
+        for s in range(0, len(status)):
+            if status[s]:
+                prev_corner1.append(prev_corner[s])
+                cur_corner1.append(cur_corner[s])
+
+        print "Frame: " + str(k) +  "/" + str(nbFrames) +  " - good optical flow: "  + str(len(prev_corner1));
+
+    print "Done"
+    return prev_to_cur_transform
 
